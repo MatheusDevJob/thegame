2ab77427c675c0be8945f1bd923b50053b11f397
Matheus Henrique <matheuseevee@gmail.com>
2026-06-08 20:36:33 -0300

    efeito abrir baú

--- Entities/WorldObjects/Interactables/Bau.cs
+++ Entities/WorldObjects/Interactables/Bau.cs
@@ -1,5 +1,5 @@
-using System;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using thegame.Core;
 
 namespace thegame.Entities.WorldObjects.Interactables;
@@ -25,6 +25,30 @@ public class Bau : Entity
         if (IsPlayerFartherThanMe(1))
             CloseBau();
     }
+
+    public override void Draw(SpriteBatch spriteBatch)
+    {
+        base.Draw(spriteBatch);
+
+        if (!Aberto) return;
+
+        Rectangle sourceTampa = new(
+            SpriteColumn * FrameWidth,
+            (SpriteRow - 1) * FrameHeight,
+            FrameWidth,
+            FrameHeight
+        );
+
+        Rectangle destinationTampa = new(
+            (int)(Posicao.X + DrawOffset.X),
+            (int)(Posicao.Y + DrawOffset.Y - FrameHeight),
+            FrameWidth,
+            FrameHeight
+        );
+
+        spriteBatch.Draw(Sprite, destinationTampa, sourceTampa, Color.White);
+    }
+
 
     public void OpenBau()
     {
